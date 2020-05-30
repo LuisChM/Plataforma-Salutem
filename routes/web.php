f312eba74c8fefe07966b80547ff8458a8f41752
Luis Chavarria <luisfelipechavama1498@gmail.com>
2020-05-30 01:12:30 -0600

    Primera parte del front

---
+++ routes/web.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('home');
+});
+
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+
+
+//plantillas base
+Route::view('iniciarSesion' , 'layouts.iniciarSesion')->name('iniciarSesion');
+
+Route::view('dashboard' , 'plantilla')->name('dashboard');
+
+Route::view('agregar' , 'dashboard.administracion.control_costo_gasto.agregar')->name('agregar');
+
+ 
