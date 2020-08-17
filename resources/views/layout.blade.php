aaecc0c309f0d3c228bce5cc214e6945f0d1c939
Luis <luisfelipechavam1498@gmail.com>
2020-08-17 01:32:13 -0600

    asset agregado

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -5,8 +5,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="/css/app.css">
-    <link rel="stylesheet" href="/css/normalize.css">
+    <link rel="stylesheet" href="{{asset('/css/app.css')}}">
+    <link rel="stylesheet" href="{{asset('/css/normalize.css')}}">
 
     <title>@yield('title','Salutem')</title>
 </head>
@@ -35,6 +35,6 @@
     </div>
 </body>
 
-<script src="/js/app.js"></script>
+<script src="{{asset('/js/app.js')}}"></script>
 
 </html>
--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -9,8 +9,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-<link rel="stylesheet" href="{{asset('/css/app.css')}}">
-    <link rel="stylesheet" href="{{asset('/css/normalize.css')}}">
+<link rel="stylesheet" href="{{asset('css/app.css')}}">
+    <link rel="stylesheet" href="{{asset('css/normalize.css')}}">
 
     <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
 
@@ -171,6 +171,6 @@
 
 </body>
 
-<script src="{{asset('/js/app.js')}}"></script>
+<script src="{{asset('js/app.js')}}"></script>
 
 </html>
