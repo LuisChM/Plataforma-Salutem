dcd8d3bac2f49e5b81a4dda1117be264a35f1ae7
Luis <luisfelipechavam1498@gmail.com>
2020-11-21 00:14:12 -0600

    Primer avance plan nutricional

---
+++ resources/views/nutricion/planNutricional/_form.blade.php
@@ -0,0 +1,194 @@
+@extends('layouts.dashboard')
+@section('seccion')
+    Pacientes
+@endsection
+
+@section('contenido')
+    <div class="container mt-5">
+        <div class="responsive-table">
+            <table class="table mt-4 text-center">
+                <thead class="thead-green">
+                    <tr>
+                        <th scope="col">Lunes</th>
+                        <th scope="col">Martes</th>
+                        <th scope="col">Miércoles</th>
+                        <th scope="col">Jueves</th>
+                        <th scope="col">Viernes</th>
+                        <th scope="col">Sabádo</th>
+                        <th scope="col">Domingo</th>
+
+                    </tr>
+                </thead>
+                <tbody class="bg-white text-left ">
+                    {{-- Desayuno --}}
+                    <tr>
+                        <td class="border-right border-top-0 ">
+                            <h5>Desayuno</h5>
+                            <textarea name="" id="" cols="20" rows="10"></textarea>
+                        </td>
+                        <td class="border-right border-top-0 ">
+                            <h5>Desayuno</h5>
+                            <textarea name="" id="" cols="20" rows="10"></textarea>
+                        </td>
+                        <td class="border-right border-top-0 ">
+                            <h5>Desayuno</h5>
+                            <textarea name="" id="" cols="20" rows="10"></textarea>
+                        </td>
+                        <td class="border-right border-top-0 ">
+                            <h5>Desayuno</h5>
+                            <textarea name="" id="" cols="20" rows="10"></textarea>
+                        </td>
+                        <td class="border-right border-top-0 ">
+                            <h5>Desayuno</h5>
+                            <textarea name="" id="" cols="20" rows="10"></textarea>
+                        </td>
+                        <td class="border-right border-top-0 ">
+                            <h5>Desayuno</h5>
+                            <textarea name="" id="" cols="20" rows="10"></textarea>
+                        </td>
+                        <td class="border-right border-top-0 ">
+                            <h5>Desayuno</h5>
+                            <textarea name="" id="" cols="20" rows="10"></textarea>
+                        </td>
+                    </tr>
+                    {{-- /Desayuno --}}
+
+                    {{-- Merienda matutina --}}
+                    <tr>
+                        <td class="border-right border-top-0 ">
+                            <h5>Merienda matutina</h5>
+                            <textarea name="" id="" cols="20" rows="10"></textarea>
+                        </td>
+                        <td class="border-right border-top-0 ">
+                            <h5>Merienda matutina</h5>
+                            <textarea name="" id="" cols="20" rows="10"></textarea>
+                        </td>
+                        <td class="border-right border-top-0 ">
+                            <h5>Merienda matutina</h5>
+                            <textarea name="" id="" cols="20" rows="10"></textarea>
+                        </td>
+                        <td class="border-right border-top-0 ">
+                            <h5>Merienda matutina</h5>
+                            <textarea name="" id="" cols="20" rows="10"></textarea>
+                        </td>
+                        <td class="border-right border-top-0 ">
+                            <h5>Merienda matutina</h5>
+                            <textarea name="" id="" cols="20" rows="10"></textarea>
+                        </td>
+                        <td class="border-right border-top-0 ">
+                            <h5>Merienda matutina</h5>
+                            <textarea name="" id="" cols="20" rows="10"></textarea>
+                        </td>
+                        <td class="border-right border-top-0 ">
+                            <h5>Merienda matutina</h5>
+                            <textarea name="" id="" cols="20" rows="10"></textarea>
+                        </td>
+                    </tr>
+                    {{-- /Merienda matutina --}}
+
+                    {{-- Almuerzo --}}
+                    <tr>
+                        <td class="border-right border-top-0 ">
+                            <h5>Almuerzo</h5>
+                            <textarea name="" id="" cols="20" rows="10"></textarea>
+                        </td>
+                        <td class="border-right border-top-0 ">
+                            <h5>Almuerzo</h5>
+                            <textarea name="" id="" cols="20" rows="10"></textarea>
+                        </td>
+                        <td class="border-right border-top-0 ">
+                            <h5>Almuerzo</h5>
+                            <textarea name="" id="" cols="20" rows="10"></textarea>
+                        </td>
+                        <td class="border-right border-top-0 ">
+                            <h5>Almuerzo</h5>
+                            <textarea name="" id="" cols="20" rows="10"></textarea>
+                        </td>
+                        <td class="border-right border-top-0 ">
+                            <h5>Almuerzo</h5>
+                            <textarea name="" id="" cols="20" rows="10"></textarea>
+                        </td>
+                        <td class="border-right border-top-0 ">
+                            <h5>Almuerzo</h5>
+                            <textarea name="" id="" cols="20" rows="10"></textarea>
+                        </td>
+                        <td class="border-right border-top-0 ">
+                            <h5>Almuerzo</h5>
+                            <textarea name="" id="" cols="20" rows="10"></textarea>
+                        </td>
+                    </tr>
+                    {{-- /Almuerzo --}}
+
+                    {{-- Merienda tarde --}}
+                    <tr>
+                        <td class="border-right border-top-0 ">
+                            <h5>Merienda tarde</h5>
+                            <textarea name="" id="" cols="20" rows="10"></textarea>
+                        </td>
+                        <td class="border-right border-top-0 ">
+                            <h5>Merienda tarde</h5>
+                            <textarea name="" id="" cols="20" rows="10"></textarea>
+                        </td>
+                        <td class="border-right border-top-0 ">
+                            <h5>Merienda tarde</h5>
+                            <textarea name="" id="" cols="20" rows="10"></textarea>
+                        </td>
+                        <td class="border-right border-top-0 ">
+                            <h5>Merienda tarde</h5>
+                            <textarea name="" id="" cols="20" rows="10"></textarea>
+                        </td>
+                        <td class="border-right border-top-0 ">
+                            <h5>Merienda tarde</h5>
+                            <textarea name="" id="" cols="20" rows="10"></textarea>
+                        </td>
+                        <td class="border-right border-top-0 ">
+                            <h5>Merienda tarde</h5>
+                            <textarea name="" id="" cols="20" rows="10"></textarea>
+                        </td>
+                        <td class="border-right border-top-0 ">
+                            <h5>Merienda tarde</h5>
+                            <textarea name="" id="" cols="20" rows="10"></textarea>
+                        </td>
+                    </tr>
+                    {{-- /Merienda tarde --}}
+
+                    {{-- Cena --}}
+                    <tr>
+                        <td class="border-right border-top-0 ">
+                            <h5>Cena</h5>
+                            <textarea name="" id="" cols="20" rows="10"></textarea>
+                        </td>
+                        <td class="border-right border-top-0 ">
+                            <h5>Cena</h5>
+                            <textarea name="" id="" cols="20" rows="10"></textarea>
+                        </td>
+                        <td class="border-right border-top-0 ">
+                            <h5>Cena</h5>
+                            <textarea name="" id="" cols="20" rows="10"></textarea>
+                        </td>
+                        <td class="border-right border-top-0 ">
+                            <h5>Cena</h5>
+                            <textarea name="" id="" cols="20" rows="10"></textarea>
+                        </td>
+                        <td class="border-right border-top-0 ">
+                            <h5>Cena</h5>
+                            <textarea name="" id="" cols="20" rows="10"></textarea>
+                        </td>
+                        <td class="border-right border-top-0 ">
+                            <h5>Cena</h5>
+                            <textarea name="" id="" cols="20" rows="10"></textarea>
+                        </td>
+                        <td class="border-right border-top-0 ">
+                            <h5>Cena</h5>
+                            <textarea name="" id="" cols="20" rows="10"></textarea>
+                        </td>
+                    </tr>
+                    {{-- /Cena --}}
+
+                </tbody>
+
+            </table>
+
+        </div>
+    </div>
+@endsection
